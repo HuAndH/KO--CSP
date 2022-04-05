b7d3dff4b72b080456cec122536ab6dd9e36cc3c
HuAndH <2353307984@qq.com>
2022-04-05 18:57:29 +0800

    Modified KO--CSP\PAT\javaSrc\PAT-B1020.月饼 .java

--- PAT/javaSrc/PAT-B1020.月饼 .java
+++ PAT/javaSrc/PAT-B1020.月饼 .java
@@ -1,58 +1,123 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Main {
-    static class Cake implements Comparable<Cake> {
-        double amount;
-        double price;
-        double value;
+public class Mooncake002 {
+    public static void main(String[] args){
 
-        public Cake(double amount, double price, double value) {
-            this.amount = amount;
-            this.price = price;
-            this.value = value;
+//        3 20       //月饼种类数 int number   市场需求量 double marketDemand
+//        18 15 10   //n种月饼的库存量(t) double mooncake[]
+//        75 72 45   //n种月饼全部售空各种月饼所得收益(亿元) double money[]
+//
+//        要求输出最高收益： double highestYield;
+
+        //信息描述
+
+        System.out.println("3 20       //月饼种类数 int number   市场需求量 double marketDemand");
+        System.out.println("18 15 10   //n种月饼的库存量(wt) double[] mooncake");
+        System.out.println("75 72 45   //n种月饼全部售空各种月饼所得收益(亿元) double[] money");
+        System.out.println("请按上述所给示例输入信息：");
+        int number;  //月饼种类数
+        double marketDemand;  //市场需求量
+        double highestYield;  //最高收益
+
+        //数据采集
+
+        Scanner scanner = new Scanner(System.in);
+
+        //月饼种类数
+//        不超过 1000 的正整数表示月饼的种类数
+        while(true){
+            number = scanner.nextInt();
+            if(number < 0 | number >=1000){
+                System.out.println("月饼种类数输入错误！");
+                System.out.print("请输入[1,10000]之间的整数: ");
+            }
+            else break;
         }
 
-        @Override
-        public int compareTo(Cake o) {
-            return value > o.value ? -1 : 1;
+        double[] mooncake=new double[number];  //n种月饼的库存量(wt)
+        double[] money= new double[number];  //n种月饼全部售空各种月饼所得收益(亿元)
+
+        //市场需求量
+//        不超过 500（以万吨为单位）的正整数 D 表示市场最大需求量。
+        while(true){
+            marketDemand = scanner.nextDouble();
+            if(marketDemand  > 500  | marketDemand < 1)
+            {
+                System.out.println("市场需求量输入错误！");
+                System.out.print("请输入[0,500]之间的数: ");
+            }
+            else break;
         }
 
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        String[] in = br.readLine().split(" ");
-        int n = Integer.parseInt(in[0]);
-        int d = Integer.parseInt(in[1]);
-
-        ArrayList<Cake> list = new ArrayList<Cake>();
-        String[] w = br.readLine().split(" "), pr = br.readLine().split(" ");
-        br.close();
-
-        for (int i = 0; i < n; i++) {
-            double m = Double.parseDouble(w[i]);
-            double p = Double.parseDouble(pr[i]);
-            double value = p / m;
-            list.add(new Cake(m, p, value));
-        }
-
-        Collections.sort(list);
-
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            if (d - list.get(i).amount >= 0) {
-                sum += list.get(i).price;
-                d -= list.get(i).amount;
-            } else {
-                sum += list.get(i).value * d;
-                break;
+        //n种月饼的库存量(wt)
+        for(int i=1 ; i <= number ; i++)
+        {
+            mooncake[i-1] = scanner.nextDouble();
+            if(mooncake[i-1] < 0)
+            {
+                System.out.println(number+"种月饼的库存量输入错误！");
+                System.out.println("请重新输入第"+i+"个数据：");
+                i--;
             }
         }
-        System.out.println(String.format("%.2f", sum));
+
+        //n种月饼全部售空各种月饼所得收益(亿元)
+        for(int i=1 ; i <= number ; i++){
+            money[i-1] = scanner.nextDouble();
+        }
+
+        //运行 结果
+
+        Method001 method001 = new Method001();
+        highestYield=method001.zuiZuanQian(marketDemand,mooncake,money);
+        System.out.println("最高收益为："+highestYield);
+    }
+}
+
+
+class Method001{
+    public double zuiZuanQian(double marketDemand,double[] mooncake,double[] money ){
+        //新建 double[] unitPrice 单价(亿元/t)
+        //计算各种月饼的单价
+        //map 排序
+        //减法。okk
+
+        //新建 double[] unitPrice 单价(亿元/t)
+        double best=0;    //最高收益
+        double[] unitPrice = new double[mooncake.length];
+        double[] mcake = new double[mooncake.length];
+        double[] moy = new double[mooncake.length];
+        for(int i = 1 ; i <= mooncake.length ; i++)
+            mcake[i-1] = mooncake [i-1];
+        for(int i = 1 ; i <= mooncake.length ; i++)
+            moy[i-1] = money[i-1];
+
+        //计算各种月饼的单价
+        for(int i=1 ; i <= mooncake.length ; i++){
+            unitPrice[i-1] = moy[i-1] / mcake[i-1];
+        }
+
+        //map 排序
+        Arrays.sort(unitPrice);   //自带的排序  ↑
+
+        //减法。okk
+        for(int i = mooncake.length ; i >= 0 ; i--)
+        {
+            if(marketDemand >= 0 )
+            {
+                if(marketDemand >= mooncake[i-1])
+                    best += money[i-1];
+                else{
+                    best += (mooncake[i-1]-marketDemand) * unitPrice[i-1];
+                    return best;
+                }
+                marketDemand = marketDemand - mooncake[i-1];
+            }
+        }
+
+        return best;
     }
 }
